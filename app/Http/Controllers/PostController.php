d3b349690197c3b54b17f16b6f0fdf43229a2699
Ali A. Dhillon <ali.dhillon1@hotmail.com>
2019-07-28 15:28:02 +0500

    Added edit responce in PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -70,7 +70,7 @@ class PostController extends Controller
      */
     public function edit(Post $post)
     {
-        //
+        return response('This is the edit method of PostController');
     }
 
     /**
